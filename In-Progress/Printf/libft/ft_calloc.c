c6c043dea57215b4011c4b0295de612fc3b4abaf
Elias LARBI <ellarbi@e3r1p7.42.fr>
2019-12-16 14:11:48 +0100

    finito capito gnl & add libft to printf

---
+++ In-Progress/Printf/libft/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ellarbi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/05 19:47:23 by ellarbi           #+#    #+#             */
+/*   Updated: 2019/11/05 19:47:24 by ellarbi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == 0)
+		return (ptr);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
---
+++ In-Progress/Printf/libft/ft_strlcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ellarbi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/31 15:01:54 by ellarbi           #+#    #+#             */
+/*   Updated: 2019/08/31 18:22:45 by ellarbi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t j;
+	size_t k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (dst[i])
+		i++;
+	while (src[k])
+		k++;
+	if (dstsize <= i)
+		k += dstsize;
+	else
+		k += i;
+	while (src[j] && i + 1 < dstsize)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (k);
+}
