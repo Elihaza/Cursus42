1fd64608f41a962f154b0156a0d25fbc0deb0dbe
Elihaza <elias.larbi@gmail.com>
2020-11-19 15:26:09 +0100

    Deleted libasm + woopsi cub3d

--- Libftdir/libft/ft_strlcpy.c
+++ Libftdir/libft/ft_strlcpy.c
@@ -13,7 +13,7 @@
 #include <stdio.h>
 #include "libft.h"
 
-size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	lensrc;
 
